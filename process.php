ba1b9f84819ba92213d87474dbf7f1b8b1b21e30
Friedrich Große <friedrich.grosse@gmail.com>
2013-05-19 18:50:16 +0200

    Introduced a limit on the maximum input size (prevent slowing the whole server)

--- process.php
+++ process.php
@@ -6,25 +6,33 @@
 require_once 'phpasn1/classes/PHPASN_Autoloader.php';
 PHPASN_Autoloader::register();
 
-$binaryData = base64_decode($_POST['input']);
-$asnObject = ASN_Object::fromBinary($binaryData);
+$maxInputSize = 12540;
 
-function printObject(ASN_Object $object, $depth=0) {
-    $treeSymbol = '';
-    $depthString = str_repeat('─', $depth);
-    if($depth > 0) {
-        $treeSymbol = '├';
-    }
+if(strlen($_POST['input']) > $maxInputSize) {
+    echo "Sorry but an input size greater than {$maxInputSize} will not be processed. Input size was " . strlen($_POST['input']) . "<br>";
+    echo "<b>Note</b>: You can contact me via <a href='mailto:friedrich.grosse@gmail.com'>email</a> if you have some good reasons why I should change the limit<br><br><br>";
+}
+else {
+    $binaryData = base64_decode($_POST['input']);
+    $asnObject = ASN_Object::fromBinary($binaryData);
+
+    function printObject(ASN_Object $object, $depth=0) {
+        $treeSymbol = '';
+        $depthString = str_repeat('─', $depth);
+        if($depth > 0) {
+            $treeSymbol = '├';
+        }
+        
+        $name = Identifier::getShortName($object->getType());
+        echo "{$treeSymbol}{$depthString}<b>{$name}</b> : ";
+        
+        echo $object->__toString() . '<br/>';
     
-    $name = Identifier::getShortName($object->getType());
-    echo "{$treeSymbol}{$depthString}<b>{$name}</b> : ";
-    
-    echo $object->__toString() . '<br/>';
-    
-    $content = $object->getContent();
-    if(is_array($content)) {        
-        foreach ($object as $child) {
-            printObject($child, $depth+1);
+        $content = $object->getContent();
+        if(is_array($content)) {        
+            foreach ($object as $child) {
+                printObject($child, $depth+1);
+            }
         }
     }    
 }
